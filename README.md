da1b36e4fb018b3a24ac853d275ba9cbf3c098ba
tommwatson <tom.m.watson@gmail.com>
2018-04-05 14:10:52 +1000

    chore(*): initial README and setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pyomo-orm
+
+pyomo-orm uses SQLAlchemy to add ORM-like data handling for Pyomo.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pyomo-orm',
+    version='0.1',
+    description='ORM like functionality for Pyomo and SQLAlchemy',
+    url='',
+    author='mattswoon',
+    packages=[
+        'pyomo_orm',
+        'pyomo_orm.core'
+    ],
+    install_requires=[
+        'SQLAlchemy>=1.2',
+        'Pyomo>=5.2'
+    ]
+)
